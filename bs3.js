e99e7ede6940e1c9c99e0c13a085cdf2464c2986
Benjamin Huang <coolconfucius@yahoo.com>
2015-09-13 14:51:33 -0700

    added oppoenent grid, the placeShipAI feature and improved colors

--- bs3.js
+++ bs3.js
@@ -1,43 +1,62 @@
 $(document).ready(init);
 
-var grid = [
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '], 
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '], 
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] '],
-['[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ', '[ ] ']
-];
+var grid = [[], [], [], [], [], [], [], [], [], []];
+for (var i = 0; i < grid.length; i++) {
+  for (var j = 0; j < 10; j++) {
+    grid[i].push('O');    
+  };
+};
+
+var grid2 = [[], [], [], [], [], [], [], [], [], []];
+for (var i = 0; i < grid2.length; i++) {
+  for (var j = 0; j < 10; j++) {
+    grid2[i].push('O');    
+  };
+};
 
 var row;
 var col; 
 var ships = [2, 3, 3, 4, 5]; //17. 
 var limit = 5; 
-var state = "select ship";
+var state = "click join";
 var size; 
 var shipId; 
 var shipCoords = []; 
 var $state = $('#state');
 var $coordR = $('#coordR');
 var $coordC = $('#coordC');
+
 function init() {
   $state.text(state);
-  
+  // placeShipAI();
   $('button').click(buttonClick); 
+};
+
+function placeShipAI(){
+  var ships = 5; 
+  var row = Math.floor((Math.random() * 10)); 
+  var col = Math.floor((Math.random() * 10));  
+  for (var i = 0; i < ships; i++) { 
+    while (grid2[row][col] === 'S'){
+      row = Math.floor((Math.random() * 10));
+      col = Math.floor((Math.random() * 10));  
+    }
+    grid2[row][col] = 'S';
+    $('#bx'+row+col).text("S");
+    $('#bx'+row+col).addClass("occupied");      
+  };
 };
 
 function buttonClick(){
   var $this = $(this);
+  if ($this.hasClass('join')) {
+    placeShipAI(); 
+    state = "select ship";
+  };
   if($this.hasClass('ship')){
     if (state === "select ship") {
       size = parseInt($this.text());
       shipId = $this[0].id;
-
-      
       state = "place ship";
       $state.text(state);
     };
@@ -64,6 +83,7 @@ function buttonClick(){
       if (grid[row][col] !== "S") {
         grid[row][col] = "S";
         $('#box'+row+col).text("S");
+        $('#box'+row+col).addClass("occupied");
         limit -= 1; 
         row = '';
         col = '';
@@ -86,11 +106,15 @@ function buttonClick(){
 
     } else           
     if (state === "select target") {
-      if (grid[row][col] !== "M" || grid[row][col] !== "H") {
-        if (grid[row][col] === "S") {
-          grid[row][col] = "H";
+      if (grid2[row][col] !== "M" || grid2[row][col] !== "H") {
+        if (grid2[row][col] === "S") {
+          grid2[row][col] = "H";
+          $('#bx'+row+col).text("H");
+          $('#bx'+row+col).addClass("hit");
         } else {
-          grid[row][col] = "M";
+          grid2[row][col] = "M";
+          $('#bx'+row+col).text("M");
+          $('#bx'+row+col).addClass("miss");
         }
       }
         
